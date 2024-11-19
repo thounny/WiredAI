d11f195e0456aaa4108e7fa02f1e49a0910483dd
thounny <thounnykeo@gmail.com>
2024-11-18 21:58:49 -0500

    Register page | Connect with WiredAI

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,12 @@
 // NODE MODULES
-import { Link } from "react-router-dom";
+import { Link, Form } from "react-router-dom";
 
 // CUSTOM MODULES
-import { banner, iconLogo, logoLight, logoDark } from "../assets/assets";
+import { logoLight, logoDark } from "../assets/assets";
 
 // COMPONENTS
 import PageTitle from "../components/PageTitle";
+import TextField from "../components/TextField";
 
 const Register = () => {
   return (
@@ -17,6 +18,22 @@ const Register = () => {
           <img src={logoLight} width={133} height={23} alt="WiredAI logo" className="" />
           <img src={logoDark} width={133} height={23} alt="WiredAI logo" className="" />
           </Link>
+
+          <div className="">
+            <h2 className="">Create an Account</h2>
+
+            <p className="">
+              Register today to connect with WiredAI—your gateway to The Wired.
+            Tap into powerful AI tools that transcend boundaries and bring your ideas to life.
+            </p>
+
+            <Form
+              method="POST"
+              className=""
+            >
+              <TextField />
+            </Form>
+          </div>
         </div>
     </div>
   </>
