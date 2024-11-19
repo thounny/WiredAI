b7d5c1f997151fd2fc2021f5752b50b2dbe8fe75
thounny <thounnykeo@gmail.com>
2024-11-19 15:29:53 -0500

    Updated for input field to grab full name

--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -22,6 +22,7 @@ const TextField = ({
             <input
                 className={`text-field ${fieldClasses}`}
                 id={name}
+                name={name}
                 placeholder={placeholder} 
                 {...rest}
             />
