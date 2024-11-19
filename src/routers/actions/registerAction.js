204ad05d6121ed087e5c3e0c6e1f7a7866c306c0
thounny <thounnykeo@gmail.com>
2024-11-19 15:31:51 -0500

    Grab Data from form and Generate unique ID

--- src/routers/actions/registerAction.js
+++ src/routers/actions/registerAction.js
@@ -1,12 +1,41 @@
 // CUSTOM MODULES
-import { account } from "../lib/appwrite";
+import { account } from "../../lib/appwrite";
+import generateID from "../../utils/generateID";
 
 // REGISTER ACTION
-const registerAction = aysnc ({request}) => {
-    // RETRIEVE DATA
-    const formData = await request.formData();
-    console.dir(formData);
-    return null;
+const registerAction = async ({ request }) => {
+  // RETRIEVE DATA
+  const formData = await request.formData();
+
+  try {
+    // CREATE USER
+    await account.create(
+      generateID(), // GENERATES UNIQUE ID
+      formData.get("email"),
+      formData.get("password"),
+      formData.get("name")
+    );
+
+    // SUCCESS MESSAGE
+    return {
+      success: true,
+      message: "User registered successfully!",
+    };
+  } catch (err) {
+    if (err.code === 409) {
+      // HANDLE EMAIL ALREADY EXISTS
+      return {
+        success: false,
+        message: "A user with this email already exists. Please try logging in.",
+      };
+    }
+
+    // HANDLE OTHER ERRORS
+    return {
+      success: false,
+      message: `An error occurred: ${err.message}`,
+    };
+  }
 };
 
 export default registerAction;
