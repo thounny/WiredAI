d607472cfb2640a61518971135c41b6b7fbb77f1
thounny <thounnykeo@gmail.com>
2024-11-18 21:09:03 -0500

    RouterProvider | Setup custom modules

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
+// @copyright 2024 thounny
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
+// CUSTOM MODULES
+import router from './routers/routes'
+
+// CSS LINK
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
