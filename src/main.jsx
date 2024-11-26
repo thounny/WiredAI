330f2347354b93c252581803c071263fee9789bf
thounny <thounnykeo@gmail.com>
2024-11-26 14:17:20 -0500

    Imported SnackbarProvider

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,16 @@ import { RouterProvider } from 'react-router-dom'
 // CUSTOM MODULES
 import router from './routers/routes'
 
+// COMPONENTS
+import SnackbarProvider from './contexts/SnackbarContext';
+
 // CSS LINK
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SnackbarProvider>
     <RouterProvider router={router} />
+    </SnackbarProvider>
   </StrictMode>,
 );
