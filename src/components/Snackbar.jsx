6c17e195de98f44de463c5d3af6c0ff2fb115897
thounny <thounnykeo@gmail.com>
2024-11-26 15:15:43 -0500

    add Snackbar component with Framer Motion animations

--- src/components/Snackbar.jsx
+++ src/components/Snackbar.jsx
@@ -1,14 +1,44 @@
 import PropTypes from "prop-types";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Snackbar = ({snackbar}) => {
+    // FRAMER MOTION VARIANTS
+    const snackbarVariant = {
+        hidden: {scaleY: 0},
+        visible: {
+            scaleY: 1,
+            transition: {
+                duration: 0.2,
+                ease: [0.05, 0.7, 0.1, 1],
+            }
+        }
+    }
+
+    const snackbarChildVariant = {
+        hidden: { opacity: 0},
+        visible: { opacity: 1}
+    }
     return (
-        <>
+        <AnimatePresence>
         {snackbar.open && (
-        <div className={`snackbar ${snackbar.type}`}>
-            <span>{snackbar.message}</span>
-        </div>
+        <motion.div 
+            variants={snackbarVariant} 
+            initial="hidden"
+            animate="visible"
+            exit={{
+                opacity: 1,
+                transition: {
+                    duration: 0.15,
+                    ease: "easeOut",
+                },
+            }}
+            className={`snackbar ${snackbar.type}`}>
+            <motion.span variants={snackbarChildVariant}
+            transition={{ duration: 0.2, delay: 0.1, ease: "easeOut"}}
+            >{snackbar.message}</motion.span>
+        </motion.div>
          )}
-        </>
+        </AnimatePresence>
     )
 }
 
