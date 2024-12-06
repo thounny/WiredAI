8fd0cdadf88a49842bd6ff2f4fb9247d0502c4b4
thounny <thounnykeo@gmail.com>
2024-12-06 14:09:27 -0500

    Setup buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,5 @@
 // NODE MODULES
+import { button } from "framer-motion/client";
 import PropTypes from "prop-types";
 
 // BUTTON COMPONENT
@@ -26,4 +27,29 @@ Button.propTypes = {
     children: PropTypes.any,
 }
 
-export {Button};
+// ICON BUTTON
+const IconBtn = ({ classes = "", icon, size = "", children, ...rest }) => {
+    return (
+        <button className={`icon-btn ${size} ${classes}`} {...rest}>
+
+            {children}
+
+            {!children && (
+                <span className={`material-symbols-rounded icon-${size}`}>
+                    {icon}
+                </span>
+            )}
+
+            <div className="state-layer"></div>
+        </button>
+    )
+}
+
+IconBtn.propTypes = {
+    classes: PropTypes.string,
+    icon: PropTypes.string,
+    size: PropTypes.string,
+    children: PropTypes.any,
+}
+
+export {Button, IconBtn};
