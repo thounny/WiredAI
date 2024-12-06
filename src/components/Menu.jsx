16202220326d1e3a26b9cafc127892204aafa052
thounny <thounnykeo@gmail.com>
2024-12-06 14:10:45 -0500

    Created and setup Menu and Menu items

---
+++ src/components/Menu.jsx
@@ -0,0 +1,13 @@
+// NODE MODULES
+import PropTypes from "prop-types";
+
+const Menu = ({classes, children}) => {
+    return <div className={`menu ${classes}`}>{children}</div>
+}
+
+Menu.propTypes = {
+    classes: PropTypes.string,
+    children: PropTypes.any,
+}
+
+export default Menu;
