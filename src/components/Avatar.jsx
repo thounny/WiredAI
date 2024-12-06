70b1d992358496a5daf2bd03ca566db75b504f13
thounny <thounnykeo@gmail.com>
2024-12-06 14:10:14 -0500

    Avatar setup and styling

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,24 @@
+// NODE MODULES
+import PropTypes from "prop-types";
+
+// CUSTOM MODULES
+import { avatars } from "../lib/appwrite";
+
+const Avatar = ({name}) => {
+    return (
+        <figure className="avatar">
+            <img 
+                src={avatars.getInitials(name, 48, 48)}
+                alt={name} 
+                width={48}
+                height={48}
+            />
+        </figure>
+    )
+}
+
+Avatar.PropTypes = {
+    name: PropTypes.string
+}
+
+export default Avatar
