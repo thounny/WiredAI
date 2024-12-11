33adaecec9e858e911712f7cb90ad79a89cd56f0
thounny <thounnykeo@gmail.com>
2024-12-11 16:21:39 -0500

    Added user prompt action for handling form data requests

---
+++ src/routers/actions/appAction.js
@@ -0,0 +1,22 @@
+// CUSTOM MODULES
+import { account } from "../../lib/appwrite";
+
+const userPromptAction = async (formData) => {
+    const userPrompt = formData.get("user_prompt");
+
+    // GET CURRENT USER ACCOUNT DATA
+    const user = await account.get();
+
+    return null;
+};
+
+// HANDLE INCOMING REQUEST BASED ON REQUEST METHOD
+const appAction = async ({ request }) => {
+    const formData = await request.formData();
+    const requestType = formData.get("request_type");
+
+    if (requestType === "user_prompt") 
+        return await userPromptAction(formData);
+};
+
+export default appAction;
