6ed57f371938c6f467319253ae9d7f5651ca089c
thounny <thounnykeo@gmail.com>
2024-12-11 15:40:55 -0500

    Placeholder text prompt

--- src/components/PromptField.jsx
+++ src/components/PromptField.jsx
@@ -1,19 +1,62 @@
 // NODE MODULES
-import { motion } from "framer-motion";
+import { motion, stagger } from "framer-motion";
+import { useRef, useCallback, useState } from "react";
 
 // COMPONENTS
 import { IconBtn } from "./Button";
 
 const PromptField = () => {
-  return (
-    <motion.div className="prompt-field-container">
+
+    // INPUTFIELD AND INPUTFIELD CONTAINER HOLD REFERENCES TO DOM ELEMENTS
+    const inputField = useRef();
+    const inputFieldContainer = useRef();
+
+    // STATE TO DETERMINE IF PLACEHOLDER SHOULD BE SHOWN
+    const [placeholderShown, setPlaceholderShown] = useState(true);
+
+    // HANDLE INPUT FIELD INPUT CHANGE
+    const handleInputChange = useCallback(() => {
+        setPlaceholderShown(!inputField.current.innerText);
+    }, []);
+
+    // FRAMER MOTION VARIANTS FOR PROMPT FIELD ANIMATION
+    const promptFieldVariant = {
+        hidden: { scaleX: 0},
+        visible: {
+            scaleX: 1,
+            transition: {
+                when: "beforeChildren",
+                staggerChildren: 0.2,
+                duration: 0.4,
+                ease: [0.05, 0.7, 0.1, 1]
+            }
+        },
+    };
+
+    // FRAMER MOTION VARIANTS FOR PROMPT FIELD CONTAINER ANIMATION
+    const promptFieldChildrenVariant = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 },
+    };
+
+return (
+    <motion.div 
+    className="prompt-field-container"
+    variants={promptFieldVariant}
+    initial="hidden"
+    animate="visible"
+    ref={inputFieldContainer}
+    >
         <motion.div 
-            className="prompt-field"
+            className={`prompt-field ${placeholderShown ? "" : "after:hidden"}`}
             contentEditable={true}
             role="textbox"
             aria-multiline={true}
             aria-label="Enter your prompt here"
             data-placeholder="Enter your prompt here"
+            variants={promptFieldChildrenVariant}
+            ref={inputField}
+            onInput={handleInputChange}
         />
 
         <IconBtn 
@@ -21,6 +64,7 @@ const PromptField = () => {
             title="Submit" 
             size="large" 
             classes="ms-auto" 
+            variants={promptFieldChildrenVariant}
         />
 
         <div className="state-layer"></div>
