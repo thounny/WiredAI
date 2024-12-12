abec8162ac4dd91117bb00a315c5aa7fa2ace014
thounny <thounnykeo@gmail.com>
2024-12-12 10:55:57 -0500

    Implement PromptPreloader component with UserPrompt, AiResponse, and Skeleton placeholders

---
+++ src/components/PromptPreloader.jsx
@@ -0,0 +1,25 @@
+// NODE MODULES
+import PropTypes from "prop-types";
+
+// COMPONENTS
+import UserPrompt from "./UserPrompt";
+import AiResponse from "./AiResponse";
+import Skeleton from "./Skeleton";
+
+const PromptPreloader = ({promptValue}) => {
+  return (
+    <div className="max-w-[700px] mx-auto">
+        <UserPrompt text={promptValue}/>
+
+        <AiResponse>
+            <Skeleton />
+        </AiResponse>
+    </div>
+  );
+};
+
+PromptPreloader.propTypes = {
+    promptValue: PropTypes.string,
+};
+
+export default PromptPreloader
