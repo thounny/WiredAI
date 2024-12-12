f05272aecb20bd6634b39c0f7556ae344934a913
thounny <thounnykeo@gmail.com>
2024-12-11 22:26:56 -0500

    Imported hopscotch style with prism

--- src/components/AiResponse.jsx
+++ src/components/AiResponse.jsx
@@ -2,11 +2,66 @@
 import PropTypes from "prop-types";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { hopscotch, coy } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+// COMPONENTS
+
 
 // ASSETS
 import { iconLogo } from "../assets/assets";
 
+
+
 const AiResponse = ({ aiResponse, children }) => {
+
+    // FUNCTION EXECUTES FOR EVERY CODE TAG
+    const code = ({ children, className, ...rest }) => {
+        const match = className?.match(/language-(\w+)/);
+
+        return match ? (
+            <>
+                <div className="code-block">
+                    <div className="">{match[0]}</div>
+
+                    <SyntaxHighlighter
+                    {...rest}
+                    PreTag="div"
+                    language={match[1]}
+                    style={hopscotch}
+                    customStyle={{
+                        marginBlock: "0",
+                        padding: "2px",
+                    }}
+                    codeTagProps={{
+                        style: {
+                            padding: "14px",
+                            fontWeight: "600",
+                        },
+                    }}
+                    >
+                        {children}
+                    </SyntaxHighlighter>
+                </div>
+
+                <div className="">
+                    <p>
+                        Use code
+                        <a 
+                            className="link ms-2"
+                            href="https://gemini.google.com/faq#coding"
+                            target="_blank"
+                            >
+                                with caution.
+                        </a>
+                    </p>
+                </div>
+            </>
+        ) : (
+            <code className={className}>{children}</code>
+        )
+    };
+
   return (
     <div className="grid grid-cols-1 items-start gap-1 py-4 
     md:grid-cols-[max-content,minmax(0,1fr)]
@@ -24,7 +79,8 @@ const AiResponse = ({ aiResponse, children }) => {
         {children}
 
     <div className="markdown-content">
-            <Markdown remarkPlugins={[remarkGfm]}>
+            <Markdown remarkPlugins={[remarkGfm]} 
+            components={{ code }}>
                 {aiResponse}
             </Markdown>
         </div>
