765bffa9c9d8f772dcd2550d07e4ced33336666d
thounny <thounnykeo@gmail.com>
2024-12-11 21:29:23 -0500

    Display user and AI response

--- src/pages/Conversation.jsx
+++ src/pages/Conversation.jsx
@@ -4,6 +4,8 @@ import { useLoaderData } from "react-router-dom"
 
 // COMPONENTS
 import PageTitle from "../components/PageTitle"
+import UserPrompt from "../components/UserPrompt"
+import AiResponse from "../components/AiResponse"
 
 const Conversation = () => {
     const { conversation: {title, chats },
@@ -13,6 +15,18 @@ const Conversation = () => {
     <>
         {/* Meta Title */}
         <PageTitle title={`${title} | WiredAI`} />
+
+        <motion.div className="">
+            {chats.map((chat) => (
+                <div key={chat.$id}>
+                    {/* USER PROMPT */}
+                    <UserPrompt text={chat.user_prompt} />
+                
+                {/* AI RESPONSE */}
+                <AiResponse aiResponse={chat.ai_response} />
+                </div>
+            ))}
+        </motion.div>
     </>
   )
 }
